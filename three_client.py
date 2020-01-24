9f8c6df09459167fa42c098aa560f197532f80b8
Daliatalal <60266537+Daliatalal@users.noreply.github.com>
2020-01-24 08:29:59 -0800

    Add files via upload

---
+++ three_client.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+host = '127.0.0.1'
+send_port = 9999
+recv_port = 8080
+def send():
+    while True:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((host, send_port))
+        msg = input("Enter your message :   ")
+        if msg == "over":
+            client.send(msg)
+            client.close()
+            listen()
+        else:
+            client.send(msg)
+def listen():
+    print("listenning ...\n")
+    serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serv.bind((host, recv_port))
+    serv.listen(5)
+    while True:
+        conn, addr = serv.accept()
+        while True:
+            data = conn.recv(4096)
+            if not data: break
+            elif data == "over":
+                conn.close()
+                serv.close()
+                print("closed server and claled send() \n")
+                send()
+            else:
+               print("server : " + data + "\n")
+        conn.close()
+send()
